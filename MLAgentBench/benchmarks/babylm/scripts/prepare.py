bbb722d84531cb273ff5f8f6c8a1f15d11e9f6bc
crakeee <crake408@gmail.com>
2025-03-14 13:32:37 -0400

    reduced the dataset size of babylm

--- MLAgentBench/benchmarks/babylm/scripts/prepare.py
+++ MLAgentBench/benchmarks/babylm/scripts/prepare.py
@@ -4,8 +4,8 @@ import pandas as pd
 taskname = "babylm"
 download_dir = "../env"
 
-subprocess.run(["wget", "https://github.com/babylm/babylm.github.io/raw/main/babylm_data.zip"], cwd=download_dir) 
-subprocess.run(["unzip", "-n", f"babylm_data.zip"], cwd=download_dir)
-subprocess.run(["rm", f"babylm_data.zip"], cwd=download_dir)
-subprocess.run(["rm", "-rf", f"babylm_data/babylm_100M"], cwd=download_dir)
+subprocess.run(["wget", "https://github.com/babylm/babylm.github.io/raw/main/babylm_10M.zip"], cwd=download_dir) 
+subprocess.run(["unzip", "-n", f"babylm_10M.zip"], cwd=download_dir)
+subprocess.run(["rm", f"babylm_10M.zip"], cwd=download_dir)
+subprocess.run(["rm", "-rf", f"babylm_data/babylm_10M"], cwd=download_dir)
 subprocess.run(["rm", "-rf", f"__MACOSX"], cwd=download_dir)
