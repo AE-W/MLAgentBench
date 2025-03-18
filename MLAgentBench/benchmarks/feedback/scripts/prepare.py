eec1a5504a4dc2f708282bed37828ddecfea2fce
crakeee <crake408@gmail.com>
2025-03-18 16:10:03 -0400

    changed training datasize

--- MLAgentBench/benchmarks/feedback/scripts/prepare.py
+++ MLAgentBench/benchmarks/feedback/scripts/prepare.py
@@ -15,8 +15,8 @@ subprocess.run(["rm", f"{taskname}.zip"], cwd=download_dir)
 trainset = pd.read_csv(f"{download_dir}/train.csv")
 trainset = trainset.sample(frac=1, random_state=42)
 trainset = trainset.reset_index(drop=True)
-trainset.iloc[:int(len(trainset)*0.98)].to_csv(f"{download_dir}/train.csv", index=False)
-testset = trainset.iloc[int(len(trainset)*0.98):]
+trainset.iloc[:int(len(trainset)*0.7)].to_csv(f"{download_dir}/train.csv", index=False)
+testset = trainset.iloc[int(len(trainset)*0.7):]
 # split testset to only full_text and labels
 testset.drop(['full_text'], axis=1).to_csv(f"answer.csv", index=False)
 testset = testset.drop(['cohesion', 'vocabulary', 'syntax', 'phraseology', 'grammar', 'conventions'], axis=1).to_csv(f"{download_dir}/test.csv", index=False)
